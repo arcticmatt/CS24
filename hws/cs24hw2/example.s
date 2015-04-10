2f53690776771e100f9c9c15a0ed4b3e60ee6855
Matt Lim <mlimcollege@gmail.com>
2015-04-10 12:10:58 -0700

    Fix example.s (compile on 32 bit machine)

--- hws/cs24hw2/example.s
+++ hws/cs24hw2/example.s
@@ -1,31 +1,30 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.globl	_ex
-	.align	4, 0x90
-_ex:                                    ## @ex
+	.file	"example.c"
+	.text
+	.globl	ex
+	.type	ex, @function
+ex:
+.LFB0:
 	.cfi_startproc
-## BB#0:
-	pushq	%rbp
-Ltmp2:
-	.cfi_def_cfa_offset 16
-Ltmp3:
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-Ltmp4:
-	.cfi_def_cfa_register %rbp
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	movl	%edx, -12(%rbp)
-	movl	%ecx, -16(%rbp)
-	movl	-4(%rbp), %ecx
-	movl	-8(%rbp), %edx
-	subl	-12(%rbp), %edx # This is the - operation, where the two operands are b and c
-	imull	%edx, %ecx      # This is the * operation, where the two operands are a and (b - c)
-	addl	-16(%rbp), %ecx # This is the + operation, where the two operands are d and (a * (b - c))
-	movl	%ecx, -20(%rbp)
-	movl	-20(%rbp), %eax
-	popq	%rbp
-	retq
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	12(%ebp), %eax
+	subl	16(%ebp), %eax # This is the - operation, where the two operands are b and c
+	imull	8(%ebp), %eax  # This is the * operation, where the two operands are a and (b - c)
+	movl	%eax, %edx
+	movl	20(%ebp), %eax
+	addl	%edx, %eax     # This is the + operation, where the two operands are d and (a * (b - c))
+	movl	%eax, -4(%ebp)
+	movl	-4(%ebp), %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
 	.cfi_endproc
-
-
-.subsections_via_symbols
+.LFE0:
+	.size	ex, .-ex
+	.ident	"GCC: (GNU) 4.9.2 20150304 (prerelease)"
+	.section	.note.GNU-stack,"",@progbits
