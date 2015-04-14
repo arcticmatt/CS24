d9e1e5b687dc7636961e8deea8eec460dd3eae04
Matt Lim <mlimcollege@gmail.com>
2015-04-14 10:50:24 -0700

    Fix gcdmain

--- hws/cs24hw2/gcdmain.c
+++ hws/cs24hw2/gcdmain.c
@@ -6,22 +6,27 @@ int min(int x, int y);
 int main(int argc, char **argv) {
     int arg1 = -1;
     int arg2 = -1;
+    int pass1, pass2;
     int result;
 
-    if (argc == 3) {
+    // Check for correct number of arguments
+    if (argc > 1)
         arg1 = atoi(argv[1]);
+    if (argc == 3) {
         arg2 = atoi(argv[2]);
-        arg1 = max(arg1, arg2);
-        arg2 = min(arg1, arg2);
+        // Sort arguments we pass into gcd
+        pass1 = max(arg1, arg2);
+        pass2 = min(arg1, arg2);
     }
 
-    if (arg1 >= 0 && arg2 >= 0) {
-        printf("Calling gcd(%d, %d)\n", arg1, arg2);
-        result = gcd(arg1, arg2);
-        printf("gcd(%d, %d) = %d\n", arg1, arg2, result);
+    // Only call gcd on positive arguments
+    if (arg1 > 0 && arg2 > 0) {
+        printf("Calling gcd(%d, %d)\n", pass1, pass2);
+        result = gcd(pass1, pass2);
+        printf("gcd(%d, %d) = %d\n", pass1, pass2, result);
     } else {
         printf("Invalid number of args or invalid args, arg1 = %d, \
-                arg2 = %d, argc = %d\n", arg1, arg2, argc);
+                arg2 = %d, argc = %d\n", pass1, pass2, argc);
     }
 }
 
