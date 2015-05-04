965917ea1dc79f0e7d8717df01af31792d8e57ad
Matt Lim <mlimcollege@gmail.com>
2015-05-03 17:59:02 -0700

    Finish pixel.s

--- hws/cs24mid/screen/pixel.s
+++ hws/cs24mid/screen/pixel.s
@@ -14,27 +14,27 @@
 #
 #
 draw_pixel:
-    push   %ebp              # Set up the stack
-    mov    %esp, %ebp
+    pushl  %ebp              # Set up the stack
+    movl   %esp, %ebp
 
     # Save callee-save registers we alter
-    push   %ebx
-    push   %esi
-    push   %edi
+    pushl  %ebx
+    pushl  %esi
+    pushl  %edi
 
     # Get width and height of screen
-    mov    8(%ebp), %edx     # %edx = address of screen array, dereferencing
+    movl   8(%ebp), %edx     # %edx = address of screen array, dereferencing
                              # should give us the width
-    mov    (%edx), %ebx      # %ebx = M[%edx] = width
-    add    $4, %edx          # %edx += 4, dereferencing should give us the
+    movl   (%edx), %ebx      # %ebx = M[%edx] = width
+    addl   $4, %edx          # %edx += 4, dereferencing should give us the
                              # height
-    mov    (%edx), %esi      # %esi = M[%edx] = height
-    add    $4, %edx          # %edx += 4, dereferencing should give us the
+    movl   (%edx), %esi      # %esi = M[%edx] = height
+    addl   $4, %edx          # %edx += 4, dereferencing should give us the
                              # first pixel
 
     # Calculate index of screen array to access to get the pixel we want
-    mov    12(%ebp), %edi    # %edi = x
-    mov    16(%ebp), %ecx    # %ecx = y
+    movl   12(%ebp), %edi    # %edi = x
+    movl   16(%ebp), %ecx    # %ecx = y
 
     # Make sure pixel coordinates are within bounds
     cmpl   $0, %edi          # Update flags as for x - 0
@@ -48,37 +48,40 @@ draw_pixel:
 
     # Calculate index of screen array to access to get the pixel we want
     imull  %ebx, %ecx        # %ecx = y * width
-    add    %edi, %ecx        # %ecx = y * width + x
+    addl   %edi, %ecx        # %ecx = y * width + x
+    imull  $2, %ecx          # Multiply index by 2 because each pixel is 2 bytes
 
-    add    %ecx, %edx        # %edx += y * width + x, dereferencing should give
+    addl   %ecx, %edx        # %edx += y * width + x, dereferencing should give
                              # us value of the pixel at coordinate (x, y)
-    mov    (%edx), %ebx      # %ebx = M[%edx] = old_pixel->value
-    add    $1, %edx          # %edx +=1, dereferencing should give us the depth
+    movl   %edx, %esi        # %esi = %edx; will now use %esi to access array
+    movb   (%esi), %bl       # %bl = M[%esi] = old_pixel->value
+    addl   $1, %esi          # %esi +=1, dereferencing should give us the depth
                              # of the pixel at coordinate (x, y)
-    mov    (%edx), %esi      # %esi = M[%edx] = old_pixel->depth
-    mov    20(%ebp), %ecx    # %ecx = new_pixel->value
-    mov    24(%ebp), %edi    # %edi = new_pixel->depth
-    cmpl   %edi, %esi        # Updates flags as for %esi - %edi =>
+    movb   (%esi), %dl       # %dl = M[%esi] = old_pixel->depth
+    movb   20(%ebp), %cl     # %cl = new_pixel->value
+    movb   24(%ebp), %al     # %al = new_pixel->depth
+    movzbl %dl, %edi         # "Cast" al and dl to their unsigned values
+    movzbl %al, %ebx
+    cmpl   %ebx, %edi        # Updates flags as for %dl - %al, if they were
+                             # cast to unsigned chars =>
                              # old_pixel->depth - new_pixel->depth
     jge    replace_pixel     # If old_pixel->depth >= new_pixel->depth, replace
                              # the pixel
     jmp    draw_pixel_return # Else, just return
 
 replace_pixel:
-    sub    $1, %edx          # Move %edx back so that it points to
+    movb   %al, (%esi)       # Change the pixel depth to the passed-in value
+    subl   $1, %esi          # Move %esi back so that it points to
                              # old_pixel->value
-    mov    %ecx, (%edx)      # Change the pixel value to the passed-in value
-    add    $1, %edx          # Move %edx forward so that it points to
-                             # old_pixel -> depth
-    mov    %edi, (%edx)      # Change the pixel depth to the passed-in value
+    movb   %cl, (%esi)       # Change the pixel value to the passed-in value
 
 draw_pixel_return:
     xor    %eax, %eax        # Return 0
 
     # Restore the callee-save registers we altered
-    pop    %edi
-    pop    %esi
-    pop    %ebx
+    popl   %edi
+    popl   %esi
+    popl   %ebx
 
     # Clean up stack, return
     movl   %ebp, %esp
