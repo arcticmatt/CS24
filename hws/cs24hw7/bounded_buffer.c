5789d760fc8bf5eadffd58cc31a5bd780a93ebad
Matt Lim <mlimcollege@gmail.com>
2015-05-28 20:19:31 -0700

    Initial implementation of problem 5

--- hws/cs24hw7/bounded_buffer.c
+++ hws/cs24hw7/bounded_buffer.c
@@ -13,6 +13,7 @@
 
 #include "sthread.h"
 #include "bounded_buffer.h"
+#include "semaphore.h"
 
 /*
  * The bounded buffer data.
@@ -29,6 +30,12 @@ struct _bounded_buffer {
 
     /* The values in the buffer */
     BufferElem *buffer;
+
+    /*
+     * Semaphores, used for making bounded buffer thread safe.
+     */
+    Semaphore *semp_add;     // locks based on add
+    Semaphore *semp_take;    // locks based on take
 };
 
 /*
@@ -63,6 +70,26 @@ BoundedBuffer *new_bounded_buffer(int length) {
     bufp->length = length;
     bufp->buffer = buffer;
 
+    /*
+     * NEW - allocate the sempahores
+     */
+    /* Start this one out with a length equivalent to how many items we can
+     * store in our buffer. We do this because we will decrement this semp
+     * everytime something gets added - thus, when the maximum number of
+     * elements get added, we not be able to add anything until something
+     * is taken
+     */
+    Semaphore *semp_add = new_semaphore(length);
+    /*
+     * Start this one out with a length of zero. We do this because initially,
+     * the buffer has nothing in it. And since we decrement this semp in
+     * the take method, we want it to block initially.
+     */
+    Semaphore *semp_take = new_semaphore(0);
+
+    bufp->semp_add = semp_add;
+    bufp->semp_take = semp_take;
+
     return bufp;
 }
 
@@ -71,13 +98,20 @@ BoundedBuffer *new_bounded_buffer(int length) {
  * thread if the buffer is full.
  */
 void bounded_buffer_add(BoundedBuffer *bufp, const BufferElem *elem) {
-    /* Wait until the buffer has space */
-    while (bufp->count == bufp->length)
-        sthread_yield();
+    /*
+     * If there is space, we will add to the buffer. Else, this call will wait.
+     */
+    semaphore_wait(bufp->semp_add);
 
     /* Now the buffer has space */
     bufp->buffer[(bufp->first + bufp->count) % bufp->length] = *elem;
     bufp->count++;
+
+    /*
+     * Upon succesfully adding an element, increment the take semaphore because
+     * we know we can take one more item from this buffer.
+     */
+    semaphore_signal(bufp->semp_take);
 }
 
 /*
@@ -85,14 +119,21 @@ void bounded_buffer_add(BoundedBuffer *bufp, const BufferElem *elem) {
  * thread if the buffer is empty.
  */
 void bounded_buffer_take(BoundedBuffer *bufp, BufferElem *elem) {
-    /* Wait until the buffer has a value to retrieve */
-    while (bufp->count == 0)
-        sthread_yield();
+    /*
+     * If there is an element to take, we will take it. Else, this call will
+     * wait
+     */
+    semaphore_wait(bufp->semp_take);
 
     /* Copy the element from the buffer, and clear the record */
     *elem = bufp->buffer[bufp->first];
     bufp->buffer[bufp->first] = empty;
     bufp->count--;
     bufp->first = (bufp->first + 1) % bufp->length;
-}
 
+    /*
+     * Upon succesfully taking an element, increment the add semaphore because
+     * we know we can add one more item to this buffer.
+     */
+    semaphore_signal(bufp->semp_add);
+}
